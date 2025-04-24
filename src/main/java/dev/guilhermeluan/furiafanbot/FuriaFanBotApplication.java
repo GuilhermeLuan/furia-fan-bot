f094e2cce2b8c6df0ba8d3246177d9b96b47a50b
Guilherme Luan <guilhermeluan@pm.me>
2025-04-24 15:34:39 -0300

    first commit

---
+++ src/main/java/dev/guilhermeluan/furiafanbot/FuriaFanBotApplication.java
@@ -0,0 +1,13 @@
+package dev.guilhermeluan.furiafanbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuriaFanBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuriaFanBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/guilhermeluan/furiafanbot/FuriaFanBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilhermeluan.furiafanbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuriaFanBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
