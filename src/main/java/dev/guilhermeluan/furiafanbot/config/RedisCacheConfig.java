996f0a5439d2a89bde7cab213dbccdc3b652e5f6
Guilherme Luan <guilhermeluan@pm.me>
2025-04-28 18:07:55 -0300

    feat: redis cache config

---
+++ src/main/java/dev/guilhermeluan/furiafanbot/config/RedisCacheConfig.java
@@ -0,0 +1,31 @@
+package dev.guilhermeluan.furiafanbot.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+import java.time.Duration;
+
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
+
+@Configuration
+public class RedisCacheConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Jdk8Module());
+        return mapper;
+    }
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+}
